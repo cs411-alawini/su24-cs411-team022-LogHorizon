d28fa9bf2dfa0ecee3590806c83430d22071aee7
root <root@inst-builder-debian-12-build-build-7w84n.us-central1-a.c.gce-image-builder.internal>
2024-07-25 18:06:17 +0000

    Mike's repo

---
+++ gameRec/app.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql2');
+var path = require('path');
+var connection = mysql.createConnection({
+                host: '34.27.8.241',
+                user: 'root',
+                password: 'abc123',
+                database: 'GameRecommender'
+});
+
+connection.connect;
+
+var app = express();
+
+// set up ejs view engine 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+ 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '../public'));
+
+/* GET home page, respond by rendering index.ejs */
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Mark Attendance' });
+});
+
+app.get('/success', function(req, res) {
+      res.send({'message': 'Attendance marked successfully!'});
+});
+ 
+// this code is executed when a user clicks the form submit button
+app.post('/mark', function(req, res) {
+  var netid = req.body.netid;
+   
+  var sql = `INSERT INTO attendance (netid, present) VALUES ('${netid}',1)`;
+
+
+
+console.log(sql);
+  connection.query(sql, function(err, result) {
+    if (err) {
+      res.send(err)
+      return;
+    }
+    res.redirect('/success');
+  });
+});
+
+app.get('/api/attendance', function(req, res) {
+  var sql = 'SELECT * FROM attendance';
+
+  connection.query(sql, function(err, results) {
+    if (err) {
+      console.error('Error fetching attendance data:', err);
+      res.status(500).send({ message: 'Error fetching attendance data', error: err });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.
+
+app.post('/login', async (req, res) => {
+  try{
+      let foundUser = users.find((data) => req.body.email === data.email);
+      if (foundUser) {
+  
+          let submittedPass = req.body.password; 
+          let storedPass = foundUser.password; 
+  
+          const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+          if (passwordMatch) {
+              let usrname = foundUser.username;
+              res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+          } else {
+              res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+          }
+      }
+      else {
+  
+          let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+          await bcrypt.compare(req.body.password, fakePass);
+  
+          res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+      }
+  } catch{
+      res.send("Internal server error");
+  }
+});
+
+module.exports = app;
---
+++ gameRec/server.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+
+const PORT = 80;
+
+app.listen(PORT, function () {
+    console.log('Node app is running on port 80');
+});
