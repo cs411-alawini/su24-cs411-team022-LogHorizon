d7780381353bcb1c7e447118b97fc9b0ecb90c12
root <root@inst-builder-debian-12-build-build-7w84n.us-central1-a.c.gce-image-builder.internal>
2024-07-30 20:03:26 +0000

    updated app.js

--- gameRec/app.js
+++ gameRec/app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const query = (sql) => {
   return new Promise((resolve, reject) => {
